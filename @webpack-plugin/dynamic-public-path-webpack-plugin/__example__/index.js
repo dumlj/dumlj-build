843d6d117be0ddff6ff1a1a2d97f91715d5db423
DavidJones <qowera@gmail.com>
2023-11-12 17:27:59 +0800

    docs(custom): 更新文档

--- @webpack-plugin/dynamic-public-path-webpack-plugin/__example__/index.js
+++ @webpack-plugin/dynamic-public-path-webpack-plugin/__example__/index.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-console
 console.log(process.env.WS)
 
-document.body.innerHTML = `Open browser console.<br/>process.env.WS is ${process.env.WS}.`
+document.body.innerHTML = `Open browser console.<br/>process.env.WS is ${process.env.WS}.<br/><a href="javascript:history.back()">Back</a>`
